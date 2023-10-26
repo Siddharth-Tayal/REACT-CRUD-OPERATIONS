81ad175240ca08a0283ec580d6dc058c2b3391b7
Siddharth Tayal <125152950+Siddharth-Tayal@users.noreply.github.com>
2023-10-26 11:25:21 +0530

    Update Footer.jsx
    Updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import Founder from "../assets/logo.png"
-import { FiInstagram, FiTwitter, FiYoutube } from "react-icons/fi";
+import Founder from "../assets/logo.png";
+import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
 const Footer = () => {
   return (
     <div className="footer">
       <div className="founder">
-        <h2>Our Founder </h2>
+        <h2>Developed By </h2>
         <img src={Founder} alt="" />
         <h3>Siddharth Tayal</h3>
       </div>
       <div className="links">
-        <h2>Our Brands :</h2>
-        <a href="www.google.com">
+        <h2>Visit at :</h2>
+        <a href="https://github.com/Siddharth-Tayal/" target="_blank">
+          <FiGithub /> Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/siddharth-tayal-65203a266/www.google.com"
+          target="_blank"
+        >
+          <FiLinkedin /> LinkedIn
+        </a>
+        <a href="https://www.instagram.com/s_tayal_01/" target="_blank">
           <FiInstagram /> Instagram
-        </a>
-        <a href="www.google.com">
-          <FiTwitter /> Twitter
-        </a>
-        <a href="www.google.com">
-          <FiYoutube /> YouTube
         </a>
       </div>
     </div>
