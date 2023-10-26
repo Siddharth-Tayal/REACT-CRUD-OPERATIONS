f8295dc950f9f27e43a2ced850f588e4a4dbefa5
Siddharth Tayal <125152950+Siddharth-Tayal@users.noreply.github.com>
2023-10-26 11:41:56 +0530

    Add files via upload

---
+++ src/components/NopAGE.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BiErrorAlt } from "react-icons/bi";
+
+const NopAGE = () => {
+  return (
+    <div className="errorPage">
+      <BiErrorAlt />
+      Page Not Found
+    </div>
+  );
+};
+
+export default NopAGE;
